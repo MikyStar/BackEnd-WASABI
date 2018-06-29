1851ce1091134a7af24fa6f138ef5332c7955ee8
Mike AUBENAS <mike.aubenas@gmail.com>
2018-06-29 12:22:18 +0200

    Update user schema : Google ID unique but null ok #sparse ...
    I had a problem because my new schema was conflicted with the previous one in the mongo database => Solution : drop the database in mongo .. arsh but works

--- src/model/schemas/user.js
+++ src/model/schemas/user.js
@@ -5,31 +5,38 @@ const User = mongoose.model( 'user', new mongoose.Schema(
 	name :
 	{
 		type : String,
+		trim: true,
 		required : [ true, 'A name required' ]
 	},
 	surname :
 	{
 		type : String,
+		trim: true,
 		required : [true, 'A surname required' ]
 	},
 	mail :
 	{
 		type : String,
-		required : [true, 'A mail required ' ],
+		required : [true, 'A mail required' ],
+		trim : true,
 		unique : true
 	},
 	authentificationMethod :
 	{
 		type : String,
+		trim: true,
 		required: [true, 'You need to provide the authentification method.']
 	},
 	googleID :
 	{
-		type : String
+		type : String,
+		unique : true,
+		trim: true,
+		sparse : true // To allow having multiple null entries
 	},
 	password :
 	{
-		type : String,
+		type : String
 	}
 }));
 
