f5290bbec98425a48d3fb4fe0b4a5ac32777c8cd
Mike AUBENAS <mike.aubenas@gmail.com>
2018-07-02 11:18:59 +0200

    Redirect of Google's login gives the token

--- src/model/passportSetup.js
+++ src/model/passportSetup.js
@@ -4,65 +4,66 @@ const sensibleInformations = require('../assets/sensibleInformations');
 const User = require('../model/schemas/user');
 
 passport.use( new GoogleStrategy(
-{
-	callbackURL : '/api/auth/google/redirect',
-	clientID : sensibleInformations.GOOGLE_STRATEGY.CLIENT_ID,
-	clientSecret : sensibleInformations.GOOGLE_STRATEGY.CLIENT_SECRET
-},
-(accessToken, refreshToken, profile, done) =>
-{
-	let email;
-	(function retrieveEmail()
 	{
-
-		profile.emails.forEach( element =>
-		{
-			if( element.type == "account")
-				email = element.value;
-		});
-	})()
-
-	let name = profile.name.givenName;
-	let surname = profile.name.familyName;
-	let googleID = profile.id;
-
-	User.findOne( { googleID }, (error, user) =>
+		callbackURL : '/api/auth/google/redirect',
+		clientID : sensibleInformations.GOOGLE_STRATEGY.CLIENT_ID,
+		clientSecret : sensibleInformations.GOOGLE_STRATEGY.CLIENT_SECRET
+	},
+	(accessToken, refreshToken, profile, done) =>
 	{
-		if( !user )
+		let email;
+		(function retrieveEmail()
 		{
-			User.create( { name, surname, mail: email, authentificationMethod: 'google', googleID }, ( error, user ) =>
+
+			profile.emails.forEach( element =>
 			{
-				if ( error )
+				if( element.type == "account")
+					email = element.value;
+			});
+		})()
+
+		let name = profile.name.givenName;
+		let surname = profile.name.familyName;
+		let googleID = profile.id;
+
+		User.findOne( { googleID }, (error, user) =>
+		{
+			if( !user )
+			{
+				User.create( { name, surname, mail: email, authentificationMethod: 'google', googleID }, ( error, user ) =>
 				{
-					switch ( error.code )
+					if ( error )
 					{
-						case 11000:
-							return new Error( "This mail already exits, choose an other one." );
-						default:
-							return new Error( `An unexpected error occured, please contact us.\n\n${error}` );
+						switch ( error.code )
+						{
+							case 11000:
+								return new Error( "This mail already exits, choose an other one." );
+							default:
+								return new Error( `An unexpected error occured, please contact us.\n\n${error}` );
+						}
 					}
-				}
 
-				done(error, user);
-			} );
-		}
-		else
-			done(error, user);
-	});
+					done(null, user);
+				} );
+			}
+			else
+				done(null, user);
+		});
+	}
+));
 
-	// add to db
-
-}))
-
-passport.serializeUser( (user, done) =>
+passport.serializeUser( ( user, done ) =>
 {
-	done(null, user._id);
+	done(null, user.id);
 });
 
-passport.deserializeUser( (id, done) =>
+passport.deserializeUser( ( id, done ) =>
 {
-	User.findById(id, (error, user) =>
+	User.findById(id).then( (error, user) =>
 	{
-		done(error, user);
+		if( !error )
+			done( null, user.id );
+		else
+			done( error, null );
 	});
-});
+} );
--- src/routes/authentification.js
+++ src/routes/authentification.js
@@ -1,9 +1,31 @@
 const router = require('express').Router();
 const passport = require('passport');
+const User = require( '../model/schemas/user' );
+const tokenHandler = require( '../model/tokenHandler' );
 
-router.get( '/auth/login', (request, response) =>
+router.get( '/auth/local', (request, response) =>
 {
+	const inputedMail = request.query.mail;
+	const inputedPassword = request.query.password;
 
+	User.findOne( { mail: inputedMail }, ( error, user ) =>
+	{
+		if ( error )
+			response.status( 400 ).send( error );
+		else if ( !user )
+			response.status( 400 ).send( "There's no user signed in with that email" );
+		else
+		{
+			if ( user.password === inputedPassword )
+			{
+				tokenHandler.createToken( { "id": user.id, "name": user.name, "surname": user.surname } ).then(
+					( token ) => { response.send( token ); },
+					( error ) => { response.status( 400 ).send( `An error occured : ${error}` ); }
+				);
+			}
+			else response.status( 403 ).send( "Password incorrect" )
+		}
+	} );
 });
 
 router.get( '/auth/logout', (request, response) =>
@@ -18,7 +40,10 @@ router.get( '/auth/google', passport.authenticate('google',
 
 router.get( '/auth/google/redirect', passport.authenticate('google'), (request, response) =>
 {
-	response.send( request.user );
+	tokenHandler.createToken( { "id": request.user.id, "name": request.user.name, "surname": request.user.surname } ).then(
+		( token ) => { response.send( token ); },
+		( error ) => { response.status( 400 ).send( `An error occured : ${error}` ); }
+	);
 });
 
 module.exports = router;
