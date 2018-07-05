23c0969593a4b633208175f4d132282709bda8fe
Mike AUBENAS <mike.aubenas@gmail.com>
2018-07-05 15:52:40 +0200

    Add route for getting all banks of user (not usefull but needed to test)

--- src/routes/bankRoutes.js
+++ src/routes/bankRoutes.js
@@ -16,6 +16,20 @@ router.post( '/bank', tokenController.tokenAnalyzerMiddleware, (request, respons
 	)
 });
 
+router.get( '/bank', tokenController.tokenAnalyzerMiddleware, (request, response) =>
+{
+	tokenController.verifyTokenAndRetrieveUser(request.token).then(
+		(user) =>
+		{
+			bankController.getAll(user).then(
+				(banks) => { response.send(banks); },
+				( error ) => { response.status( 400 ).send( `An unexpected error occured : ${error}` ); }
+			)
+		},
+		( error ) => { response.status( 403 ).send( `Authentification failed : ${error}` ); }
+	);
+});
+
 router.get('/bank', tokenController.tokenAnalyzerMiddleware, (request, response) =>
 {
 	tokenController.verifyTokenAndRetrieveUser(request.token).then(
