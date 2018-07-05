3acb73d91992eb1748892ea35fab612e9522ae6e
Mike AUBENAS <mike.aubenas@gmail.com>
2018-07-05 16:10:03 +0200

    Add route for getting all connexions (test needed) -> problem solved

--- src/controller/entities/connexionController.js
+++ src/controller/entities/connexionController.js
@@ -1,5 +1,6 @@
 const userController = require( './userController' );
 const presetController = require( './bankController' );
+const presetController = require( './presetController' );
 
 module.exports =
 {
@@ -14,5 +15,28 @@ module.exports =
 				( error ) => { reject( error ); }
 			);
 		});
+	},
+
+	getAll : async (user) =>
+	{
+		return new Promise( (resolve, reject) =>
+		{
+			let arrayConnexions = [];
+
+			presetController.getAll(user).then(
+				(presets) =>
+				{
+					presets.forEach( preset =>
+					{
+						preset.connexions.forEach(connexion => arrayConnexions.push(connexion) )
+					});
+
+					resolve(arrayConnexions);
+				},
+				(error) => { reject(error); }
+			)
+		});
+	},
+
 	}
 }
--- src/routes/connexionRoutes.js
+++ src/routes/connexionRoutes.js
@@ -3,6 +3,20 @@ const tokenController = require( '../controller/tokenController' );
 const presetController = require('../controller/entities/presetController');
 const connexionController = require('../controller/entities/connexionController');
 
+router.get( '/connexion', tokenController.tokenAnalyzerMiddleware, (request, response) =>
+{
+	tokenController.verifyTokenAndRetrieveUser(request.token).then(
+		(user) =>
+		{
+			connexionController.getAll(user).then(
+				(connexions) => { response.send(connexions) },
+				( error ) => { response.status( 400 ).send( `An unexpected error occured : ${error}` ); }
+			)
+		},
+		( error ) => { response.status( 403 ).send( `Authentification failed : ${error}` ); }
+	)
+});
+
 router.post('/connexion/:presetID', tokenController.tokenAnalyzerMiddleware, (request, response) =>
 {
 	tokenController.verifyTokenAndRetrieveUser(request.token).then(
