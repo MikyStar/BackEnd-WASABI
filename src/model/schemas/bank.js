144104cb4d5e08aee91630c969a5285142d26bca
Mike AUBENAS <mike.aubenas@gmail.com>
2018-07-23 17:05:03 +0200

    Update bank and preset schemas : pedalboardID will be unique ...
    ... identifier for the pedalboard to locate them

--- src/model/schemas/bank.js
+++ src/model/schemas/bank.js
@@ -1,8 +1,16 @@
 const mongoose = require( 'mongoose' );
 const Preset = require('./preset');
+const entitiesController = require( '../../controller/entities/entitiesController' );
 
 const Bank = mongoose.model( 'bank', new mongoose.Schema(
 	{
+		pedalBoardID:
+		{
+			type: String,
+			trim: true,
+			required : true,
+			default: () => { return entitiesController.generateRandomPedalBoardID(); }
+		},
 		label:
 		{
 			type: String,
--- src/model/schemas/preset.js
+++ src/model/schemas/preset.js
@@ -1,9 +1,17 @@
 const mongoose = require( 'mongoose' );
 const Plugin = require('./plugin');
 const Connexion = require('./connexion');
+const entitiesController = require('../../controller/entities/entitiesController');
 
 const Preset = mongoose.model( 'preset', new mongoose.Schema(
 	{
+		pedalBoardID:
+		{
+			type: String,
+			trim: true,
+			required: true,
+			default: () => { return entitiesController.generateRandomPedalBoardID(); }
+		},
 		label :
 		{
 			type: String,
