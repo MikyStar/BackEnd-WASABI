afafea04497da9931ee0c00a1342211e58854469
Mike AUBENAS <mike.aubenas@gmail.com>
2018-07-31 14:20:59 +0200

    Container running but requests time out

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
 # Big thanks to this article https://goo.gl/7f3Xdr
 
 # Add NodeJS and specify it's version
-FROM node:9.3.0
+FROM node:10.6.0
+
+# Makes the directory where the code is hosted
+RUN mkdir -p /usr/src/backend-wasabi
 
 # Specify where every command in the rest of this file will e executed
 WORKDIR /usr/src/backend-wasabi
 
 # Install all the dependencies with the package.json file
 COPY package.json /usr/src/backend-wasabi
-RUN npm install
+RUN npm cache verify && npm install
 
 # Copy the code of this project to the server
 COPY . /usr/src/backend-wasabi
@@ -17,4 +20,4 @@ COPY . /usr/src/backend-wasabi
 EXPOSE 5001
 
 # The command to execute our container
-CMD npm start
+CMD [ "npm", "start" ]
--- src/model/database.js
+++ src/model/database.js
@@ -1,13 +1,23 @@
 const mongoose = require('mongoose');
 const sensibleInformations = require('../assets/sensibleInformations');
-
-const DATABASE_CONNECTION = `mongodb://${sensibleInformations.DATABASE_USER}:${sensibleInformations.DATABASE_PASSWORD}@${sensibleInformations.SERVER_ADRESS}:${sensibleInformations.DATABASE_PORT}/${sensibleInformations.DATABASE_NAME }`;
+/*
+const DATABASE_CONNECTION = `mongodb://${sensibleInformations.DATABASE_USER}:${sensibleInformations.DATABASE_PASSWORD}@${sensibleInformations.SERVER_ADRESS}:${sensibleInformations.DATABASE_PORT}/${sensibleInformations.DATABASE_NAME }`; */
+const DATABASE_CONNECTION = `mongodb://mongo:${sensibleInformations.DATABASE_PORT}`;
 
 exports.initializeMongo = () =>
 {
 	console.log(`Trying to connect to MongoDB`);
 
-	mongoose.connect( DATABASE_CONNECTION, { useNewUrlParser: true });
+	let options =
+	{
+		useNewUrlParser : true,
+		user : sensibleInformations.DATABASE_USER,
+		pass : sensibleInformations.DATABASE_PASSWORD,
+		dbName : sensibleInformations.DATABASE_NAME,
+		auth : { authdb: 'admin' }
+	};
+
+	mongoose.connect( DATABASE_CONNECTION, options);
 	mongoose.Promise = global.Promise;
 
 	const database = mongoose.connection;
