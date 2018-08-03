1b1306a4f2a39eeaadfbdf2f29a4a5daf29f5500
Mike AUBENAS <mike.aubenas@gmail.com>
2018-08-03 13:43:06 +0200

    Now I'm able to edit the future Dockerfile given a .txt template + ...
    ... add regex for SymVer

--- src/controller/regex.js
+++ src/controller/regex.js
@@ -4,17 +4,17 @@ const User = require( '../model/schemas/user' );
 
 module.exports =
 {
-	checkAlphaNumeric : (string) =>
+	checkAlphaNumeric : ( string ) =>
 	{
 		return /^[a-z0-9]+$/i.test(string);
 	},
 
-	checkEmail : (email) =>
+	checkEmail : ( email ) =>
 	{
 		return /^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/.test(email);
 	},
 
-	checkPassword : (password) =>
+	checkPassword : ( password ) =>
 	{
 		// https://www.regextester.com/3319 ; https://goo.gl/Z4et2X
 		const containsWhatWeAsked = /^(?=.*[A-Z].*[A-Z])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{6,20}$/.test( password );
@@ -23,4 +23,19 @@ module.exports =
 
 		return ( containsWhatWeAsked && !containsCharacterWeDontWant )
 	},
+
+	/**
+	 * @description Check if string matches a SymVer of three numbers separated by dots.
+	 *
+	 * @example True = '1.0.0' ; 999.999.999' ; '123.45.6'
+	 * @example False = 'a.0.0' ; '12.0' ; '122.098.O'
+	 *
+	 * @param {string} symVer The SymVer we want to check
+	 *
+	 * @returns {boolean} True if is a valid SymVer, false otherwise
+	 */
+	checkSymVer : ( symVer ) =>
+	{
+		return /^[0-9]?[0-9]?[0-9]\.[0-9]?[0-9]?[0-9]\.[0-9]?[0-9]?[0-9]$/.test( symVer);
+	}
 }
--- src/routes/pedalRoutes.js
+++ src/routes/pedalRoutes.js
@@ -1,6 +1,7 @@
 const router = require( 'express' ).Router();
 const tokenController = require( '../controller/tokenController' );
 const serverManager = require('../controller/serverManager');
+const containerController = require('../controller/containerController');
 
 // TODO after the WAC make it use tokenAnalyzerMiddleware
 router.post( '/pedal', /* tokenController.tokenAnalyzerMiddleware, */ (request, response) =>
@@ -15,11 +16,18 @@ router.post( '/pedal', /* tokenController.tokenAnalyzerMiddleware, */ (request, 
 
 
 	let npmPackageName = request.body.npmPackageName;
+	let nodeVersion = request.body.nodeVersion;
 
-	serverManager.checkIfNPMPackageAlreadyInstalled( npmPackageName ).then(
+	/* serverManager.checkIfNPMPackageAlreadyInstalled( npmPackageName ).then(
+		( resolve ) => response.send( resolve ),
+		( reject ) => response.status( 400 ).send( reject )
+	); */
+
+	containerController.createNPMTestContainer(null, 'theTest', '0.0.0').then(
 		( resolve ) => response.send( resolve ),
 		( reject ) => response.status( 400 ).send( reject )
 	);
+
 });
 
 module.exports = router;
